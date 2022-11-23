e49762fbc04d6369548f934cde14cf565a874328
Aakanksha <Aakanksha2013073@akgec.ac.in>
2022-11-24 01:13:10 +0530

    error cors

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
 const express = require('express');
-var cors = require('cors')
+const  cors = require('cors')
 require("./db/student");
 const Student = require("./model/Schema");
 const studentRouter = require('./router/studentsroutes');
 const rateLimit = require('express-rate-limit')
 const app = express();
-const PORT = process.env.PORT || 3000;
-
 app.use(
   cors({
     origin: "*",
   })
 );
+const PORT = process.env.PORT || 3000;
+
 const limiter = rateLimit({
   max: 5,
   windowMs: 10000
