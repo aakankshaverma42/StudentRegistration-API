745be3be5ff63693ddb2a6bc804c8fd9343c2e90
Aakanksha <Aakanksha2013073@akgec.ac.in>
2023-01-03 16:45:28 +0530

    next chnge

--- router/registrationRouter.js
+++ router/registrationRouter.js
@@ -7,6 +7,7 @@ const fetch = require("isomorphic-fetch");
 
 
 // Middleware for verifying the captcha
+
 const verifyCaptcha = async (req, res, next) => {
   console.log(req.body);
   const { captchaToken } = req.body;
@@ -33,70 +34,69 @@ const verifyCaptcha = async (req, res, next) => {
     });
 };
 
-// Function for registering at code auction
-const register_to_code_auction = async (req_body) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const url = "https://code-auction-backend.up.railway.app/auth/register"
+// // Function for registering at code auction
 
-      const data = {
-        team_name: req_body.team.teamname,
-        password: req_body.team.password
-      }
-      const member_data = []
 
-      req_body.team.groupA.forEach((member, index) => {
-        member_data.push({
-          name: member.name,
-          email: member.email,
-          phone: member.phoneNo,
-        })
-      })
-      req_body.team.groupB.forEach((member, index) => {
-        member_data.push({
-          name: member.name,
-          email: member.email,
-          phone: member.phoneNo,
-        })
-      })
+// const register_to_code_auction = async (req_body) => {
+//   return new Promise(async (resolve, reject) => {
+//     try {
+//       const url = "https://code-auction-backend.up.railway.app/auth/register"
 
-      member_data.forEach((member, index) => {
-        if (index == 0) {
-          data['leader_name'] = member.name
-          data['leader_email'] = member.email
-        } else {
-          data[`member_${index}_name`] = member.name
-          data[`member_${index}_email`] = member.email
-        }
-      })
+//       const data = {
+//         team_name: req_body.team.teamname,
+//         password: req_body.team.password
+//       }
+//       const member_data = []
 
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-      const json = await response.json();
-      resolve(json);
-    } catch (error) {
-      reject(error);
-    }
-  });
-};
+//       req_body.team.groupA.forEach((member, index) => {
+//         member_data.push({
+//           name: member.name,
+//           email: member.email,
+//           phone: member.phoneNo,
+//         })
+//       })
+//       req_body.team.groupB.forEach((member, index) => {
+//         member_data.push({
+//           name: member.name,
+//           email: member.email,
+//           phone: member.phoneNo,
+//         })
+//       })
+
+//       member_data.forEach((member, index) => {
+//         if (index == 0) {
+//           data['leader_name'] = member.name
+//           data['leader_email'] = member.email
+//         } else {
+//           data[`member_${index}_name`] = member.name
+//           data[`member_${index}_email`] = member.email
+//         }
+//       })
+
+//       const response = await fetch(url, {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(data),
+//       });
+//       const json = await response.json();
+//       resolve(json);
+//     } catch (error) {
+//       reject(error);
+//     }
+//   });
+// };
 
 // route for registration
-router.post('/register', verifyCaptcha, async (req, res) => {
+router.post('/register', async (req, res) => {
   try {
-    // console.log(req.body);
     const user = new Student(req.body);
     const createUser = await user.save();
 
     // After saving the data in the database, we will send the data to the code auction backend
     await register_to_code_auction(req.body);
 
-    // sendmail(createUser.team.groupA[0].email, createUser.team.groupA[0].name);
-    // sendmail(createUser.team.groupB[0].email, createUser.team.groupA[0].name);
     res.status(201).send(createUser);
   } catch (e) { console.log(e); res.status(404).send(e); }
 });
